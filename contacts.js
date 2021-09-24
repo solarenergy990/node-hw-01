8a9c5734522d73fd4eb305e0a8774a25afaaf963
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2021-09-24 12:34:53 +0200

    initializes project, installs commander + chalk

---
+++ contacts.js
@@ -0,0 +1,23 @@
+// contacts.js
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
